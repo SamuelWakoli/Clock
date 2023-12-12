3abd654d62d61fc23563066f280403ec5f32c457
Sam <swwakoli@gmail.com>
2023-12-12 20:43:04 +0300

    refined DayChip

--- app/src/main/java/com/samwrotethecode/clock/ui/presentation/app_composables/DayChip.kt
+++ app/src/main/java/com/samwrotethecode/clock/ui/presentation/app_composables/DayChip.kt
@@ -1,5 +1,6 @@
 package com.samwrotethecode.clock.ui.presentation.app_composables
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -8,11 +9,14 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,12 +26,16 @@ import com.samwrotethecode.clock.data.AlarmDatabaseItem
 @Composable
 fun DayChip(
     label: String,
+    dayIndex: Int,
     selected: Boolean,
     onClick: () -> Unit,
 ) {
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+
     Card(
         shape = CircleShape,
-        modifier = Modifier.padding(4.dp),
+        modifier = Modifier.padding(2.dp),
         border = CardDefaults.outlinedCardBorder(),
         colors = CardDefaults.cardColors(
             containerColor =
@@ -35,16 +43,31 @@ fun DayChip(
             else MaterialTheme.colorScheme.surface,
         ),
         onClick = {
+            val dayFullName = when (dayIndex) {
+                0 -> "Sunday"
+                1 -> "Monday"
+                2 -> "Tuesday"
+                3 -> "Wednesday"
+                4 -> "Thursday"
+                5 -> "Friday"
+                6 -> "Saturday"
+                else -> "Unknown"
+            }
+
             onClick()
+
+            if (selected)
+                Toast.makeText(context, "Alarm will repeat on $dayFullName", Toast.LENGTH_SHORT)
+                    .show()
         }
     ) {
         Box(
-            modifier = Modifier.size(48.dp),
+            modifier = Modifier.size(36.dp),
             contentAlignment = Alignment.Center,
         ) {
             Text(
                 text = label,
-                style = MaterialTheme.typography.titleMedium,
+                style = MaterialTheme.typography.bodyMedium,
                 fontWeight = FontWeight.Bold,
             )
         }
@@ -58,6 +81,7 @@ private fun SelectedDayChipPreview() {
         label = "S",
         selected = true,
         onClick = {},
+        dayIndex = 0,
     )
 }
 
@@ -68,6 +92,7 @@ private fun UnselectedDayChipPreview() {
         label = "S",
         selected = false,
         onClick = {},
+        dayIndex = 0,
     )
 }
 
@@ -85,13 +110,18 @@ private fun DayChipsRowPreview() {
 
     val daysLabels = listOf("S", "M", "T", "W", "T", "F", "S")
 
-    Row {
-        sampleAlarm.days.forEachIndexed { index, c ->
-            DayChip(
-                label = daysLabels[index],
-                selected = c == '1',
-                onClick = {},
-            )
-        }
-    }
+    ListItem(
+        headlineContent = {
+            Row {
+                sampleAlarm.days.forEachIndexed { index, c ->
+                    DayChip(
+                        label = daysLabels[index],
+                        dayIndex = index,
+                        selected = c == '1',
+                        onClick = {},
+                    )
+                }
+            }
+        },
+    )
 }
